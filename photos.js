e00ab22f22a1148a82d8a32310778d1d9f69cab6
eranws <eranws@gmail.com>
2017-04-14 18:50:09 -0400

    web gallery

---
+++ photos.js
@@ -0,0 +1,248 @@
+WPGgallery = new Object();
+WPGgallery.name = "מתוך חדרי הלב";
+WPGgallery.photographer = "מרים ויסנשטרן";
+WPGgallery.contact = "";
+WPGgallery.email = "";
+WPGgallery.date = "23-03-17"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "IMG_1563.jpg";
+gPhotos[0].ImageWidth = 443;
+gPhotos[0].ImageHeight = 600;
+gPhotos[0].ThumbWidth = 73;
+gPhotos[0].ThumbHeight = 100;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "IMG_1564.jpg";
+gPhotos[1].ImageWidth = 600;
+gPhotos[1].ImageHeight = 600;
+gPhotos[1].ThumbWidth = 100;
+gPhotos[1].ThumbHeight = 100;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "IMG_1565.jpg";
+gPhotos[2].ImageWidth = 600;
+gPhotos[2].ImageHeight = 579;
+gPhotos[2].ThumbWidth = 100;
+gPhotos[2].ThumbHeight = 96;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "IMG_1566.jpg";
+gPhotos[3].ImageWidth = 600;
+gPhotos[3].ImageHeight = 600;
+gPhotos[3].ThumbWidth = 100;
+gPhotos[3].ThumbHeight = 100;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "IMG_1567.jpg";
+gPhotos[4].ImageWidth = 418;
+gPhotos[4].ImageHeight = 600;
+gPhotos[4].ThumbWidth = 69;
+gPhotos[4].ThumbHeight = 100;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "IMG_1568.jpg";
+gPhotos[5].ImageWidth = 424;
+gPhotos[5].ImageHeight = 600;
+gPhotos[5].ThumbWidth = 70;
+gPhotos[5].ThumbHeight = 100;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "IMG_1569.jpg";
+gPhotos[6].ImageWidth = 600;
+gPhotos[6].ImageHeight = 540;
+gPhotos[6].ThumbWidth = 100;
+gPhotos[6].ThumbHeight = 90;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "IMG_1570.jpg";
+gPhotos[7].ImageWidth = 600;
+gPhotos[7].ImageHeight = 494;
+gPhotos[7].ThumbWidth = 100;
+gPhotos[7].ThumbHeight = 82;
+gPhotos[7].meta = new Object();
+
+gPhotos[8] = new Object();
+gPhotos[8].filename = "IMG_1571.jpg";
+gPhotos[8].ImageWidth = 600;
+gPhotos[8].ImageHeight = 474;
+gPhotos[8].ThumbWidth = 100;
+gPhotos[8].ThumbHeight = 79;
+gPhotos[8].meta = new Object();
+
+gPhotos[9] = new Object();
+gPhotos[9].filename = "IMG_1572.jpg";
+gPhotos[9].ImageWidth = 600;
+gPhotos[9].ImageHeight = 503;
+gPhotos[9].ThumbWidth = 100;
+gPhotos[9].ThumbHeight = 83;
+gPhotos[9].meta = new Object();
+
+gPhotos[10] = new Object();
+gPhotos[10].filename = "IMG_1573.jpg";
+gPhotos[10].ImageWidth = 600;
+gPhotos[10].ImageHeight = 489;
+gPhotos[10].ThumbWidth = 100;
+gPhotos[10].ThumbHeight = 81;
+gPhotos[10].meta = new Object();
+
+gPhotos[11] = new Object();
+gPhotos[11].filename = "IMG_1574.jpg";
+gPhotos[11].ImageWidth = 600;
+gPhotos[11].ImageHeight = 525;
+gPhotos[11].ThumbWidth = 100;
+gPhotos[11].ThumbHeight = 87;
+gPhotos[11].meta = new Object();
+
+gPhotos[12] = new Object();
+gPhotos[12].filename = "IMG_1577.jpg";
+gPhotos[12].ImageWidth = 462;
+gPhotos[12].ImageHeight = 600;
+gPhotos[12].ThumbWidth = 77;
+gPhotos[12].ThumbHeight = 100;
+gPhotos[12].meta = new Object();
+
+gPhotos[13] = new Object();
+gPhotos[13].filename = "IMG_1578.jpg";
+gPhotos[13].ImageWidth = 463;
+gPhotos[13].ImageHeight = 600;
+gPhotos[13].ThumbWidth = 77;
+gPhotos[13].ThumbHeight = 100;
+gPhotos[13].meta = new Object();
+
+gPhotos[14] = new Object();
+gPhotos[14].filename = "IMG_1579.jpg";
+gPhotos[14].ImageWidth = 418;
+gPhotos[14].ImageHeight = 600;
+gPhotos[14].ThumbWidth = 69;
+gPhotos[14].ThumbHeight = 100;
+gPhotos[14].meta = new Object();
+
+gPhotos[15] = new Object();
+gPhotos[15].filename = "IMG_1580.jpg";
+gPhotos[15].ImageWidth = 400;
+gPhotos[15].ImageHeight = 600;
+gPhotos[15].ThumbWidth = 66;
+gPhotos[15].ThumbHeight = 100;
+gPhotos[15].meta = new Object();
+
+gPhotos[16] = new Object();
+gPhotos[16].filename = "IMG_1581.jpg";
+gPhotos[16].ImageWidth = 394;
+gPhotos[16].ImageHeight = 600;
+gPhotos[16].ThumbWidth = 65;
+gPhotos[16].ThumbHeight = 100;
+gPhotos[16].meta = new Object();
+
+gPhotos[17] = new Object();
+gPhotos[17].filename = "IMG_1582.jpg";
+gPhotos[17].ImageWidth = 446;
+gPhotos[17].ImageHeight = 600;
+gPhotos[17].ThumbWidth = 74;
+gPhotos[17].ThumbHeight = 100;
+gPhotos[17].meta = new Object();
+
+gPhotos[18] = new Object();
+gPhotos[18].filename = "IMG_1583.jpg";
+gPhotos[18].ImageWidth = 400;
+gPhotos[18].ImageHeight = 600;
+gPhotos[18].ThumbWidth = 66;
+gPhotos[18].ThumbHeight = 100;
+gPhotos[18].meta = new Object();
+
+gPhotos[19] = new Object();
+gPhotos[19].filename = "IMG_1584.jpg";
+gPhotos[19].ImageWidth = 400;
+gPhotos[19].ImageHeight = 600;
+gPhotos[19].ThumbWidth = 66;
+gPhotos[19].ThumbHeight = 100;
+gPhotos[19].meta = new Object();
+
+gPhotos[20] = new Object();
+gPhotos[20].filename = "IMG_1585.jpg";
+gPhotos[20].ImageWidth = 400;
+gPhotos[20].ImageHeight = 600;
+gPhotos[20].ThumbWidth = 66;
+gPhotos[20].ThumbHeight = 100;
+gPhotos[20].meta = new Object();
+
+gPhotos[21] = new Object();
+gPhotos[21].filename = "IMG_1586.jpg";
+gPhotos[21].ImageWidth = 400;
+gPhotos[21].ImageHeight = 600;
+gPhotos[21].ThumbWidth = 66;
+gPhotos[21].ThumbHeight = 100;
+gPhotos[21].meta = new Object();
+
+gPhotos[22] = new Object();
+gPhotos[22].filename = "IMG_1587.jpg";
+gPhotos[22].ImageWidth = 400;
+gPhotos[22].ImageHeight = 600;
+gPhotos[22].ThumbWidth = 66;
+gPhotos[22].ThumbHeight = 100;
+gPhotos[22].meta = new Object();
+
+gPhotos[23] = new Object();
+gPhotos[23].filename = "IMG_1588.jpg";
+gPhotos[23].ImageWidth = 400;
+gPhotos[23].ImageHeight = 600;
+gPhotos[23].ThumbWidth = 66;
+gPhotos[23].ThumbHeight = 100;
+gPhotos[23].meta = new Object();
+
+gPhotos[24] = new Object();
+gPhotos[24].filename = "IMG_1589.jpg";
+gPhotos[24].ImageWidth = 400;
+gPhotos[24].ImageHeight = 600;
+gPhotos[24].ThumbWidth = 66;
+gPhotos[24].ThumbHeight = 100;
+gPhotos[24].meta = new Object();
+
+gPhotos[25] = new Object();
+gPhotos[25].filename = "IMG_1590.jpg";
+gPhotos[25].ImageWidth = 400;
+gPhotos[25].ImageHeight = 600;
+gPhotos[25].ThumbWidth = 66;
+gPhotos[25].ThumbHeight = 100;
+gPhotos[25].meta = new Object();
+
+gPhotos[26] = new Object();
+gPhotos[26].filename = "IMG_1591.jpg";
+gPhotos[26].ImageWidth = 400;
+gPhotos[26].ImageHeight = 600;
+gPhotos[26].ThumbWidth = 66;
+gPhotos[26].ThumbHeight = 100;
+gPhotos[26].meta = new Object();
+
+gPhotos[27] = new Object();
+gPhotos[27].filename = "IMG_1592.jpg";
+gPhotos[27].ImageWidth = 400;
+gPhotos[27].ImageHeight = 600;
+gPhotos[27].ThumbWidth = 66;
+gPhotos[27].ThumbHeight = 100;
+gPhotos[27].meta = new Object();
+
+gPhotos[28] = new Object();
+gPhotos[28].filename = "IMG_1593.jpg";
+gPhotos[28].ImageWidth = 400;
+gPhotos[28].ImageHeight = 600;
+gPhotos[28].ThumbWidth = 66;
+gPhotos[28].ThumbHeight = 100;
+gPhotos[28].meta = new Object();
+
